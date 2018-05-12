265301d694036c046bd2bd3f9d77ec80ad2a7696
Sven Reichel <github-sr@hotmail.com>
2018-05-12 12:57:18 +0200

    Minor change

--- src/app/code/community/StackExchange/CatalogSearch/Model/Observer.php
+++ src/app/code/community/StackExchange/CatalogSearch/Model/Observer.php
@@ -1,10 +1,19 @@
 <?php
+
+/**
+ * Class StackExchange_CatalogSearch_Model_Observer
+ */
 class StackExchange_CatalogSearch_Model_Observer
 {
-    //the product list block name in layout
+    /**
+     * the product list block name in layout
+     */
     const RESULT_BLOCK_NAME = 'search_result_list';
 
-    public function redirectToProduct($observer)
+    /**
+     * @param Varien_Event_Observer $observer
+     */
+    public function redirectToProduct(Varien_Event_Observer $observer)
     {
         /** @var Mage_Catalog_Block_Product_List $block */
         $block = Mage::app()->getLayout()->getBlock(self::RESULT_BLOCK_NAME);
@@ -15,8 +24,7 @@ class StackExchange_CatalogSearch_Model_Observer
                 $product = $collection->getFirstItem();
                 $url = $product->getProductUrl();
                 if ($url) {
-                    Mage::app()->getResponse()->setRedirect($url);
-                    Mage::app()->getResponse()->sendResponse();
+                    Mage::app()->getResponse()->setRedirect($url)->sendResponse();
                     exit; //stop everything else
                 }
             }
